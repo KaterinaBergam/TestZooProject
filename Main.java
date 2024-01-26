6da9c40a9c3cd14d424aeb0e2c8f82eb4db4959e
KaterinaBergam <157735407+KaterinaBergam@users.noreply.github.com>
2024-01-26 12:05:45 +0300

    class "MyCat" created
    Initial class and method declared

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        MyCat a = new MyCat();
+        a.Cat("katya", 2);
+
+    }
+}
---
+++ MyCat.java
@@ -0,0 +1,12 @@
+public class MyCat {
+    String name;
+    int age;
+
+    public static void constr() {
+    }
+    public static void Cat(String name, int age) {
+//        name = "Denis";
+//        age = 4;
+        System.out.println("Hi, my Cat is " + name + "and age is " + age);
+    }
+}
